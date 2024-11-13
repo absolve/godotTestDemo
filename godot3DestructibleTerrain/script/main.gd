fa3effdbd6d5978ea3a362c9aaccaa90254827db
absolve <wangding1992@126.com>
2024-11-13 10:39:46 +0800

    更新功能

---
+++ godot3DestructibleTerrain/script/main.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+
+onready var img=$Sprite
+onready var cpolygon2D=$StaticBody2D/CollisionPolygon2D
+
+func _ready():
+#	var bm = BitMap.new()
+#	bm.create_from_image_alpha(img.texture.get_data())
+#	print(img.texture.get_width())
+#	var rect = Rect2(0,0,img.texture.get_width(),img.texture.get_height())
+#	var my_array = bm.opaque_to_polygons(rect)
+#	print(my_array.size())
+#	var my_polygon = Polygon2D.new()
+#	my_polygon.set_polygons(my_array)
+#	cpolygon2D.set_polygon(my_polygon.polygon)
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
