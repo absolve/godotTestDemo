d92f29e5f166be93c16b55965d6640f5ece3a14c
absolve <wangding1992@126.com>
2024-11-18 16:03:22 +0800

    更新功能

--- godot3DestructibleTerrain/script/main.gd
+++ godot3DestructibleTerrain/script/main.gd
@@ -3,10 +3,14 @@ extends Node2D
 
 
 var spritePath="res://sprite/battle_city.png"
+var bgPath="res://sprite/Background.png"
 var tile=preload("res://scene/tile.tscn")
-var carve_radius = 40  #角度
+var ball=preload("res://scene/ball.tscn")
+var carve_radius = 20  #半径
+var ballNum=5 #球的数量
 
 onready var tiles=$tiles
+onready var balls=$balls
 
 func _ready():
 	OS.center_window()
@@ -29,7 +33,12 @@ func _ready():
 #	var temp=create_polygon_from_sprite(img)
 #	print(temp)
 
-	loadPolygonFromImg(spritePath)
+#	loadPolygonFromImg(spritePath)
+	loadPolygonFromImg(bgPath,Vector2(0,420))
+#	loadPolygonFromImg(bgPath,Vector2(320,420))
+#	loadPolygonFromImg(bgPath,Vector2(640,420))
+	
+	
 	pass
 
 # The sprite parameter must be a Sprite node.
@@ -79,8 +88,8 @@ func create_polygon_from_sprite(sprite):
 	else:
 		return false
 
-
-func loadPolygonFromImg(sprite):
+#从图片中载入
+func loadPolygonFromImg(sprite,pos:Vector2=Vector2.ZERO):
 	var texture = load(sprite)
 	var image = texture.get_data()
 
@@ -91,16 +100,20 @@ func loadPolygonFromImg(sprite):
 	print(polygons.size())
 	if polygons.size() > 0:	
 		for i in polygons:	
-			var polygon = Polygon2D.new()
-			polygon.polygon=i
-			polygon.texture=texture
-			polygon.position = Vector2.ZERO
-			add_child(polygon)
+#			var polygon = Polygon2D.new()
+#			polygon.polygon=i
+#			polygon.texture=texture
+#			polygon.position = Vector2.ZERO
+#			add_child(polygon)
 			var temp=tile.instance()
-#			temp.position = Vector2.ZERO
+			temp.position = pos
 			tiles.add_child(temp)
 			temp.setPolygon2D(i)
-
+			temp.setPolygon2DTexture(i)
+			temp.setTexture(texture)
+#	var pos1=Transform2D(0, Vector2(10,10)).xform(polygons[0])
+#	print(pos1)
+			
 #鼠标圆
 func mouseCircle():
 	var nb_points = 15
@@ -113,15 +126,53 @@ func mouseCircle():
 func _physics_process(delta):
 	if Input.is_action_pressed("click"):
 		var temp=mouseCircle()
+#		print(get_global_mouse_position())
+#		var mouse_polygon = Transform2D(0, get_global_mouse_position()).xform(temp)
 		for i in tiles.get_children():
-			var clipped_polygons=Geometry.clip_polygons_2d(i.polygon2D.polygon,temp)
-			var n_clipped_polygons = len(clipped_polygons)
-			match n_clipped_polygons:
-				0:
-					i.free()	
-				1:
-					i.setPolygon2D(clipped_polygons[0])
-				2:
-					pass
-				_:
-					i.setPolygon2D(clipped_polygons[0])	
+			if Geometry.intersect_polygons_2d(i.getPolygon2D(),temp): #判断多边形是否相交
+				var clipped_polygons=Geometry.clip_polygons_2d(i.getPolygon2D(),temp)
+				var n_clipped_polygons = len(clipped_polygons)
+				match n_clipped_polygons:
+					0:
+						i.free()	
+					1:
+						i.setPolygon2D(clipped_polygons[0])
+						i.setPolygon2DTexture(clipped_polygons[0])
+					2:
+						if Geometry.is_polygon_clockwise(clipped_polygons[0]) or \
+						Geometry.is_polygon_clockwise(clipped_polygons[1]):
+							for x in n_clipped_polygons:
+								var new=tile.instance()
+#								new.position = i.position
+								tiles.add_child(new)
+								new.setPolygon2D(clipped_polygons[x])
+								new.setPolygon2DTexture(clipped_polygons[x])
+								new.setTexture(i.getTexture())
+							i.free()
+						else:
+							i.setPolygon2D(clipped_polygons[0])
+							i.setPolygon2DTexture(clipped_polygons[0])
+							var new=tile.instance()
+							new.position = i.position
+							tiles.add_child(new)
+							new.setPolygon2D(clipped_polygons[1])
+							new.setPolygon2DTexture(clipped_polygons[1])
+#							new.setTexture(i.getTexture().duplicate())
+					_:
+						i.setPolygon2D(clipped_polygons[0])	
+						i.setPolygon2DTexture(clipped_polygons[0])
+						for x in range(n_clipped_polygons-1):
+							var new=tile.instance()
+#							new.position = i.position
+							tiles.add_child(new)
+							new.setPolygon2D(clipped_polygons[x+1])
+							new.setPolygon2DTexture(clipped_polygons[x+1])
+							new.setTexture(i.getTexture().duplicate())
+							
+				
+	elif Input.is_action_just_pressed("click_right"):
+		for i in ballNum:
+			var temp=ball.instance()
+			temp.position=get_global_mouse_position() + Vector2(randi()%20,randi()%20)
+			balls.add_child(temp)
+		
--- godot3DestructibleTerrain/script/tile.gd
+++ godot3DestructibleTerrain/script/tile.gd
@@ -2,8 +2,22 @@ extends StaticBody2D
 
 
 onready var polygon2D=$CollisionPolygon2D
-
+onready var polygon2DTexture=$Polygon2D
 
 func setPolygon2D(array):
 	polygon2D.polygon=array
 
+func setPolygon2DTexture(array):
+	polygon2DTexture.polygon=array
+
+func setTexture(texture):
+	polygon2DTexture.texture=texture
+	
+func getTexture():
+	return polygon2DTexture.texture
+
+#获取当前多边形数组 需要加上位置信息
+func getPolygon2D():
+	return Transform2D(0,position).xform(polygon2D.polygon)
+	free()
+	pass
