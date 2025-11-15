f80a5c695ecdefd4ac321e78c2b45d3a16350ce2
absolve <wangding1992@126.com>
2025-11-15 21:44:27 +0800

    更新功能

--- godot3SpriteFlip/script/main.gd
+++ godot3SpriteFlip/script/main.gd
@@ -3,19 +3,26 @@ extends Node2D
 
 var time=9  #倒计时时间
 
-onready var ani=$ani
+onready var ani=$Node2D/ani
 onready var player=$player
+onready var timer=$Timer
+
 
 func _ready():
+	OS.center_window()
 	ani.play(str(time))
-
-
+	player.play("filp")
+	
 
 #下一个计数
 func nextCount():
 	time-=1
-	if time<=0:
-		time=0
-		
-		
+	if time<0:
+		time=9
+	ani.play(str(time))	
+	player.play("back")
+	timer.start()
 
+
+func _on_Timer_timeout():
+	player.play("filp")
