11a75163eacb03c17973a47445b8f2dfdf519344
absolve <wangding1992@126.com>
2026-01-18 23:59:55 +0800

    更新功能

--- godot-4-camera-drag-zoom/script/main.gd
+++ godot-4-camera-drag-zoom/script/main.gd
@@ -10,6 +10,8 @@ var zoomSpeed = 0.1
 var mouse_start_pos = Vector2.ZERO
 var zoomToCursor=true
 var zoomPosOffset =Vector2.ZERO
+var events={}	#事件列表
+var lastDragDistance=0 #上次拖动距离
 
 func _ready():
 	pass
@@ -23,14 +25,6 @@ func _unhandled_input(_event):
 		elif Input.is_action_just_released("move"):
 			dragging = false
 
-	if OS.get_name() == "Android":
-		if _event is InputEventMagnifyGesture:
-			camera.zoom *= _event.factor
-			camera.zoom = clamp(camera.zoom, minZoom, maxZoom)
-		elif _event is InputEventPanGesture:
-			camera.offset += _event.delta
-		get_canvas_transform().affine_inverse().basis_xform(_event.position)
-
 	if dragging:
 		camera.offset += (
 			(mouse_start_pos - get_viewport().get_mouse_position()) * (1 / camera.zoom.x)
@@ -39,23 +33,56 @@ func _unhandled_input(_event):
 
 	if OS.get_name() == "Windows":
 		if Input.is_action_just_pressed("zoomIn"):
-			#print("zoomIn")
-			#camera.zoom = clamp(lerp(camera.zoom, maxZoom, zoomSpeed), minZoom, maxZoom)
-			zoom(maxZoom)
-				
+			zoom(maxZoom)	
 		if Input.is_action_just_pressed("zoomOut"):
-			#print("zoomOut")
-			#camera.zoom = clamp(lerp(camera.zoom, minZoom, zoomSpeed), minZoom, maxZoom)
 			zoom(minZoom)
+			
+	
+	if OS.get_name() == "Android":
+		if _event is InputEventScreenTouch:
+			if _event.is_pressed():
+				events[_event.index]=_event
+			else:
+				events.erase(_event.index)
+		if _event is InputEventScreenDrag:
+			events[_event.index]=_event
+			if events.size()==1:
+				camera.offset+=_event.relative*-1
+			elif events.size()==2:
+				var dragDistance=abs(events[0].position.distance_to(events[1].position))
+				if dragDistance<lastDragDistance:
+					zoom(minZoom)
+				else:
+					zoom(maxZoom)
+				lastDragDistance=dragDistance
+		
+		#在项目中设置启用平移和缩放才会生效		
+		if _event is InputEventMagnifyGesture:
+			print('InputEventMagnifyGesture')
+			camera.zoom *= _event.factor
+			camera.zoom = clamp(camera.zoom, minZoom, maxZoom)
+		if _event is InputEventPanGesture:
+			print('InputEventPanGesture')
+			camera.offset += _event.delta
+			
+	
 
 #缩放
 func zoom(value):
 	if zoomToCursor:
-		zoomPosOffset=get_global_mouse_position()
+		if  OS.get_name() == "Windows":
+			zoomPosOffset=get_local_mouse_position()
+		if OS.get_name() == "Android":
+			zoomPosOffset=get_global_mouse_position()
+			#print('1',zoomPosOffset)
 	camera.zoom = clamp(lerp(camera.zoom, value, zoomSpeed), minZoom, maxZoom)
 	if zoomToCursor:
-		camera.offset+=zoomPosOffset-get_global_mouse_position()
-
+		if  OS.get_name() == "Windows":
+			camera.offset+=zoomPosOffset-get_local_mouse_position()
+		if OS.get_name() == "Android":
+			camera.offset+=zoomPosOffset-get_global_mouse_position()
+			#print('2',get_global_mouse_position())
+			
 func _on_button_pressed() -> void:
 	camera.zoom = Vector2(1, 1)
 	camera.offset = Vector2.ZERO
