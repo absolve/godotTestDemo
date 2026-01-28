5bb227bc2772cbf49dbae9b657aa393fcf209f8e
absolve <wangding1992@126.com>
2026-01-28 14:16:16 +0800

    新增工程

---
+++ godot-4-toast/addons/my_toast/my_toast.gd
@@ -0,0 +1,22 @@
+@tool
+extends EditorPlugin
+
+
+func _enable_plugin():
+	# Add autoloads here.
+	pass
+
+
+func _disable_plugin():
+	# Remove autoloads here.
+	pass
+
+
+func _enter_tree():
+	# Initialization of the plugin goes here.
+	pass
+
+
+func _exit_tree():
+	# Clean-up of the plugin goes here.
+	pass
---
+++ godot-4-toast/addons/my_toast/script/toast_label.gd
@@ -0,0 +1,9 @@
+extends Label
+
+
+signal  remove
+
+var str:String:
+	set(value):
+		str=value
+		text=str(value)
